53b22740861f9b54964dcf05d90ce0507ce1abc9
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-05-26 15:06:54 -0300

    🔨 feat: implements resolution of challenge 8
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- ChallengeResolution.sql
+++ ChallengeResolution.sql
@@ -26,3 +26,7 @@ SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDE
 
 -- Desafio 7: Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 SELECT Ano, COUNT(*) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC;
+
+
+-- Desafio 8: Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero = 'M';
