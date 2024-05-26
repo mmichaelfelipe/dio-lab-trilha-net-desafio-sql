45ae97c2efad58ef973714d3fa4365c214935149
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-05-26 15:01:45 -0300

    🔨 feat: implements resolution of challenge 3
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- ChallengeResolution.sql
+++ ChallengeResolution.sql
@@ -3,5 +3,10 @@ USE [Filmes];
 -- Desafio 1: Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM Filmes;
 
+
 -- Desafio 2: Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
+
+
+-- Desafio 3: Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro';
