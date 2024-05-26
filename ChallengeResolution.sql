c78be0936553d771b64d2967ad891840d3e02df0
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-05-26 15:02:33 -0300

    🔨 feat: implements resolution of challenge 4
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- ChallengeResolution.sql
+++ ChallengeResolution.sql
@@ -10,3 +10,7 @@ SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
 
 -- Desafio 3: Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
 SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro';
+
+
+-- Desafio 4: Buscar os filmes lançados em 1997
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = 1997;
