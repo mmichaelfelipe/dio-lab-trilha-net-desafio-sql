0cacec6d6eef0c581ec4924795e738b650d107f6
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-05-26 14:59:17 -0300

    🔨 feat: implements resolution of challenge 2
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- ChallengeResolution.sql
+++ ChallengeResolution.sql
@@ -1,2 +1,5 @@
 -- Desafio 1: Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM Filmes;
+
+-- Desafio 2: Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
